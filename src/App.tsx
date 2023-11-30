de059323b7f75b461710f35c3927ffd14c2849b6
Bhavesh Dewangan <ybkdddd@gmail.com>
2023-11-30 12:35:30 +0530

    - Add task manager function to add, edit, delete, and mark task as done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import TaskList from "./components/TaskList";
+import AddTaskForm from "./components/AddTaskForm";
+import EditTaskForm from "./components/EditTaskForm";
+interface task {
+  id: number;
+  name: string;
+  completed: boolean;
 }
 
-export default App
+const App = () => {
+  // Load tasks from localStorage on component mount
+  const initialTasks =
+    JSON.parse(localStorage.getItem("tasks") as string) || [];
+  const [tasks, setTasks] = useState(initialTasks);
+
+  useEffect(() => {
+    // Save tasks to localStorage whenever tasks change
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleAddTask = (newTask: task) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleToggleComplete = (taskId: number) => {
+    setTasks((prevTasks: task[]) =>
+      prevTasks.map((task: task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const handleDeleteTask = (taskId: number) => {
+    setTasks((prevTasks: task[]) =>
+      prevTasks.filter((task) => task.id !== taskId)
+    );
+  };
+
+  const handleEditTask = (updatedTask: task) => {
+    setTasks((prevTasks: task[]) =>
+      prevTasks.map((task) => (task.id === updatedTask.id ? updatedTask : task))
+    );
+  };
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Task List</Link>
+            </li>
+            <li>
+              <Link to="/add">Add Task</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/add" element={<AddTaskForm onAdd={handleAddTask} />} />
+          <Route
+            path="/edit/:taskId"
+            element={<EditTaskForm tasks={tasks} onSave={handleEditTask} />}
+          />
+          <Route
+            path="/"
+            element={
+              <TaskList
+                tasks={tasks}
+                onToggleComplete={handleToggleComplete}
+                onDelete={handleDeleteTask}
+                onEdit={handleEditTask}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
